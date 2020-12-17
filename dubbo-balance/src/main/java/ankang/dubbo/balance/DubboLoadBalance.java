be49a78a0a37a5ccfa729932c689ede1a5fb36c6
ankang <ankang@soyoung.com>
2020-12-18 00:04:38 +0800

    添加dubbo 异步调用学习笔记

---
+++ dubbo-balance/src/main/java/ankang/dubbo/balance/DubboLoadBalance.java
@@ -0,0 +1,32 @@
+package ankang.dubbo.balance;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.cluster.LoadBalance;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author: ankang
+ * @email: dreedisgood@qq.com
+ * @create: 2020-12-17
+ */
+public class DubboLoadBalance implements LoadBalance {
+    @Override
+    public <T> Invoker<T> select(List<Invoker<T>> invokers , URL url , Invocation invocation) throws RpcException {
+        // 按照IP + 端口排序，取第一个
+        Collections.sort(invokers , (i1 , i2) -> {
+            int compare = i1.getUrl().getIp().compareToIgnoreCase(i2.getUrl().getIp());
+            if (compare == 0) {
+                compare = Integer.compare(i1.getUrl().getPort() , i2.getUrl().getPort());
+            }
+
+            return compare;
+        });
+
+        return invokers.get(0);
+    }
+}
